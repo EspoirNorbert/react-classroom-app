c4cebfa67016c8e351650c5f992cb8b79244459e
EspoirNorbert <espoirnorbert@gmail.com>
2021-08-23 01:09:13 +0200

    add axios for http request

--- src/Components/Classroom.js
+++ src/Components/Classroom.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { AddStudent } from './AddStudent';
 import { Student } from './Student';
+import axios from 'axios';
 
 
 export class Classroom extends Component {
@@ -8,39 +9,46 @@ export class Classroom extends Component {
     constructor(){
         super()
         this.handleDelete = this.handleDelete.bind(this)
-        this.state = {
-            students: [
-                {id:1, nom:'Ogueye Habiba Terrim'},
-                {id:2, nom:'Ouedraogo Luc'},
-                {id:3, nom:'Miraide Augusto Bassam'},
-                {id:4, nom:'Fatou Aissatou Cissé'},
-                {id:5, nom:'Aida Diao'}
-            ]
-        }
+        this.state = {students: [], error:null, isLoading:true }
+    }
+
+    componentDidMount(){
+        axios.get('http://localhost:3333/learners')
+        .then(res => {
+            const students = res.data
+            this.setState({students , isLoading: false})
+        })
+        .catch(error  => this.setState({error, isLoading: false})) 
     }
     
     handleDelete(id) {
-        this.setState(prevState => ({
-            students: prevState.students.filter(student => student.id !== id)
-        }) )
+        axios.delete('http://localhost:3333/learners/'+id)
+        .then(res => {
+            this.setState(prevState => ({
+                students: prevState.students.filter(student => student.id !== id)
+            }) )
+        })
+        .catch(error  => this.setState({error, isLoading: false}))
     }
 
     handleAdd = nom => {
-        // cretae new student object
-        const newStudent = {id: Date.now() , nom: nom}
-        // reactualiser and add student
-        this.setState({students: [ ...this.state.students,newStudent]})
+        axios.post('http://localhost:3333/learners/', {nom})
+        .then(res => {
+            this.setState({students: [ ...this.state.students,res.data]})
+            })
     }
 
 
     render() {
     const learners = this.state.students;
+    const isLoading = this.state.isLoading;
       return (
         <div>
             <AddStudent  handleAdd={this.handleAdd} />
             <h1>La liste des etudiants de  {this.props.nom} </h1>
             { (!learners.length)? <p>Aucun etudiant</p> :  <ul>
                { 
+                 (isLoading) ? <p>Loading ...</p> :
                 learners.map(learner => <Student key={learner.id} learner={learner} handleDelete={this.handleDelete  } />) 
                }
             </ul> 
