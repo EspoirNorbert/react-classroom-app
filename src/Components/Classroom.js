ab9897508ff4fdbabfc34b2ec9fa72cd4c663127
EspoirNorbert <espoirnorbert@gmail.com>
2021-08-22 21:31:34 +0200

    add cstudent and classroom component

---
+++ src/Components/Classroom.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { Student } from './Student';
+
+
+export class Classroom extends Component {
+    render() {
+      return (
+        <div>
+            <Student />
+        </div>
+      )
+    };
+}
---
+++ src/Components/Student.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+
+export  class Student extends Component {
+     
+    render() {
+        return (
+             <div>Fatou Dia</div>
+        );
+    }
+}
